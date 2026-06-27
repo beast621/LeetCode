8fe198deb1c8b9bc79b95e989fa8f610d3387346
beast621 <749564504@qq.com>
2026-06-27 16:50:47 +0800

    增加了快排算法

--- FileName.cpp
+++ FileName.cpp
@@ -41,17 +41,55 @@ void test64()
 	cout << dp[re - 1][gre - 1] << endl;
 }
 
-      
+int help(vector<int>& v, int low, int high)
+{
+	int point = v[low];
+	while(low<high)
+	{
+		while (v[high] > point && high > low) high--;
+		v[low] = v[high];
+		while (point >=v[low] && low < high) low++;
+		v[high] = v[low];
+	}
+	v[low] = point;
+	return low;
+}
+
+void Sort(vector<int>& v, int low, int high)
+{
+
+	if (high > low)
+	{
+		int point = help(v, low, high);
+
+		Sort(v,low, point - 1);
+		Sort(v, point + 1, high);
+	}
+}
+
+void QuickSort(vector<int>&v)
+{
+	int high = v.size()-1;
+	int low = 0;
+	Sort(v, low, high);
+
+	  }
     
             
-	
+void print(vector<int>v)
+{
+	for (auto x : v)
+		cout << x << "  ";
+	}
 
 
 int main()
 {
 
 	test64();
-
+	vector<int>v = { 47,46,49,46,56,5,3,8,5,1,564,66};
+	QuickSort(v);
+	print(v);
 	system("pause");
 	return 0;
 }
